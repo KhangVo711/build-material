8d80e440d41bc106f88141dff9c9db5135204adf
KhangVo711 <vhmkhang2101101@student.ctuet.edu.vn>
2024-10-19 22:16:39 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    // ADD...
-    output: 'export',  // Chỉ định xuất tĩnh
-    distDir: 'out',    // Thư mục xuất (mặc định là out)
-    images: {
-        unoptimized: true,
-      },
+    
 };
 
 export default nextConfig;
