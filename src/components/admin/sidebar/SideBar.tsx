62e7538af17985887b2fdb83e2ff20ebd80d6f89
KhangVo711 <vhmkhang2101101@student.ctuet.edu.vn>
2024-10-22 11:06:17 +0700

    fixlink

--- src/components/admin/sidebar/SideBar.tsx
+++ src/components/admin/sidebar/SideBar.tsx
@@ -2,13 +2,11 @@
 import Link from "next/link";
 import { useState } from "react";
 export default function SideBar() {
-  const [sideBarOpen, setSideBarOpen] = useState(true);
-
+  // const [sideBarOpen, setSideBarOpen] = useState(true);
+  // className={`w-1/2 md:w-1/3 lg:w-1/5 fixed md:top-0 md:left-0 h-screen lg:block bg-gray-100 z-30 ${sideBarOpen ? "" : "hidden"}`}
   return (
       <div
-        className={`w-1/2 md:w-1/3 lg:w-1/5 fixed md:top-0 md:left-0 h-screen lg:block bg-gray-100 z-30 ${
-          sideBarOpen ? "" : "hidden"
-        }`}
+        className="w-1/2 md:w-1/3 lg:w-1/5 fixed md:top-0 md:left-0 h-screen lg:block bg-gray-100 z-30"
         id="main-nav"
       >
         <div className="w-full h-16 border-b flex px-4 items-center mb-8">
@@ -51,7 +49,7 @@ export default function SideBar() {
 
           <div className="mb-4 px-4">
             <p className="pl-4 text-sm font-semibold uppercase mb-1">Sản phẩm</p>
-            <Link href="addproduct" className="w-full flex items-center text-blue-400 h-10 pl-4 hover:bg-gray-200 rounded-lg cursor-pointer">
+            <Link href="admin/addproduct" className="w-full flex items-center text-blue-400 h-10 pl-4 hover:bg-gray-200 rounded-lg cursor-pointer">
               <svg className="h-6 w-6 fill-current mr-2" viewBox="0 0 20 20">
               <path d="M14.613,10c0,0.23-0.188,0.419-0.419,0.419H10.42v3.774c0,0.23-0.189,0.42-0.42,0.42s-0.419-0.189-0.419-0.42v-3.774H5.806c-0.23,0-0.419-0.189-0.419-0.419s0.189-0.419,0.419-0.419h3.775V5.806c0-0.23,0.189-0.419,0.419-0.419s0.42,0.189,0.42,0.419v3.775h3.774C14.425,9.581,14.613,9.77,14.613,10 M17.969,10c0,4.401-3.567,7.969-7.969,7.969c-4.402,0-7.969-3.567-7.969-7.969c0-4.402,3.567-7.969,7.969-7.969C14.401,2.031,17.969,5.598,17.969,10 M17.13,10c0-3.932-3.198-7.13-7.13-7.13S2.87,6.068,2.87,10c0,3.933,3.198,7.13,7.13,7.13S17.13,13.933,17.13,10"></path>
               </svg>
